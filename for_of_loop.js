413be2246ab78e36244c364736a6bb52084321c9
Mithun Das <mithun.here01@gmail.com>
2024-02-25 01:28:14 +0530

    Adding averageOfNums program

--- for_of_loop.js
+++ for_of_loop.js
@@ -17,3 +17,14 @@ for(const user of users)
 {
     console.log(introduceUser(user));
 }
+
+const averageOfNums=function()
+{
+    let sum=0;
+    for(const argument of arguments)
+    {
+        sum+=argument;
+    }
+    return sum/arguments.length;
+};
+console.log(averageOfNums(1,2,3,4,5,6,7,8,9,10));
