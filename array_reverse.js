f802332b3d31cc86e0b25d509e16d4714c510a30
Mithun Das <mithun.here01@gmail.com>
2024-02-27 02:34:29 +0530

    Adding array_reverse file

--- array_reverse.js
+++ array_reverse.js
@@ -1,12 +1,21 @@
-const isPalindrome=(word)=>
+// const isPalindrome=(word)=>
+// {
+//     let wordArr=[];
+//     let revWord=[];
+//     for(const w of word)
+//     {
+//         wordArr.push(w);
+//         revWord.push(w);
+//     }
+//     return wordArr.toString()===revWord.reverse().toString();
+// };
+// console.log(isPalindrome('122')?'Palindrome...' :'Not Palindrome...');
+
+// const isPalindrome=(word)=>word.split('').reverse().join('')===word;
+// console.log(isPalindrome('121') ? 'It is a Palindrome...' : 'Not Palindrome...');
+
+function isPalindrome(word)
 {
-    let wordArr=[];
-    let revWord=[];
-    for(const w of word)
-    {
-        wordArr.push(w);
-        revWord.push(w);
-    }
-    return wordArr.toString()===revWord.reverse().toString();
+    return word.split('').reverse().join('')===word;
 };
-console.log(isPalindrome('122')?'Palindrome...' :'Not Palindrome...');
+console.log(isPalindrome('123')?'Palindrome' : 'Not Palindrome');
