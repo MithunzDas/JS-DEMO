85b78fade29d8db1e8465d8a0a321275e567055e
Mithun Das <mithun.here01@gmail.com>
2024-02-27 05:02:30 +0530

    Callback function

--- callback.js
+++ callback.js
@@ -2,9 +2,6 @@
 const numbers = [1, 2, 3, 4, 5];
 
 // Define a callback function
-function logArrayElements(element, index, array) {
-    console.log(`Element at index ${index}: ${element}`);
-}
-
-// Use forEach to log each element of the array
-numbers.forEach(logArrayElements);
+const squredNumbers=[];
+numbers.forEach(n=>squredNumbers.push(n*n));
+console.log(squredNumbers);
