957073aaf3594c87841861afbf276696e4ff88e4
Mithun Das <mithun.here01@gmail.com>
2024-03-03 16:10:12 +0530

    Add replace_string.js file

--- replace_string.js
+++ replace_string.js
@@ -1,3 +1,7 @@
-const str='Mithun is a good boy. Mithun gf is Mou.';
-const t=str.replaceAll('Mithun','Liton');
-console.log(t);
+// const str='Mithun is a good boy. Mithun gf is Mou.';
+// const t=str.replaceAll('Mithun','Liton');
+// console.log(t);
+
+const message='Hoogly is 200 miles from Habra and about 150  miles from kolkata';
+const messagekms=message.replace(/\d+(?!miles)/g, (miles)=>miles*1.609).replace(/miles/g,'kms');
+console.log(messagekms);
