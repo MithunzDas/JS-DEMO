87066a6acca9e30f4d0b1349c939819fff03c50b
Mithun Das <mithun.here01@gmail.com>
2024-03-05 01:12:46 +0530

    Add reverse.js file

---
+++ reverse.js
@@ -0,0 +1,17 @@
+ let A=[1,2,3,4,5];
+// console.log(A.reverse());
+let B=[];
+for(let i=A.length-1;i>=0;i--)
+{
+    B.push(A[i]);
+}
+console.log(B.toString());
+
+
+let C='Mithun Das';
+let str="";
+for(let i=C.length-1;i>=0;i--)
+{
+    str+=C[i];
+}
+console.log(str);
